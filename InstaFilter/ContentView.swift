1f56785e9e295a741a308807446ca172aa44f0e6
Olivier Van hamme <nicefiction@gmail.com>
2021-08-09 10:23:19 +0200

    Completes Day 65.1 : Building our basic UI .

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -1,21 +1,74 @@
-//
-//  ContentView.swift
-//  InstaFilter
-//
-//  Created by Olivier Van hamme on 09/08/2021.
-//
+// ContentView.swift
+
+// MARK: - LIBRARIES -
 
 import SwiftUI
 
+
+
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
+   
+   // MARK: - PROPERTY WRAPPERS
+   
+   @State private var filterIntensity: CGFloat = 0.5
+   @State private var image: Image?
+   
+   
+   
+   // MARK: - COMPUTED PROPERTIES
+   
+   var body: some View {
+      
+      NavigationView {
+         VStack {
+            ZStack {
+               Rectangle()
+                  .fill(Color.secondary)
+               // TODO: Display the image.
+            }
+            .onTapGesture {
+               // TODO: Select the image.
+               if let _image = image {
+                  _image
+                     .resizable()
+                     .scaledToFit()
+               } else {
+                  Text("Select New Image")
+                     .font(.headline)
+               }
+            }
+            HStack {
+               Text("Intensity")
+               Slider(value: $filterIntensity,
+                      in: 0...1)
+            }
             .padding()
-    }
+            HStack {
+               Button("Change Filter") {
+                  // TODO: Chenge the filter.
+               }
+               Spacer()
+               Button("Save") {
+                  // TODO: Save the image.
+               }
+            }
+         }
+         .padding([.horizontal, .bottom])
+         .navigationBarTitle("InstaFilter")
+      }
+   }
 }
 
+
+
+
+
+// MARK: - PREVIEWS -
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+   
+   static var previews: some View {
+      
+      ContentView()
+   }
 }
