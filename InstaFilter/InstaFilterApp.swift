2605a18ddb2b6ffc69e2c5d8fc5956a957b82de7
Olivier Van hamme <nicefiction@gmail.com>
2021-08-09 09:56:24 +0200

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Olivier Van hamme on 09/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
